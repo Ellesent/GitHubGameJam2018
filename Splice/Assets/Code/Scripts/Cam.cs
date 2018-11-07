61bfe6f25bbf8f8ef079fa196a41afe43a29b515
ellesent <hypahflipperz@gmail.com>
2018-11-06 17:03:20 -0800

    renaming folder + adding camera following player (needs tweaking)

---
+++ Splice/Assets/Code/Scripts/Cam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour {
+
+    [SerializeField]
+    Transform playerPos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(playerPos.position.x, playerPos.position.y, -10);
+		
+	}
+}
